2fa4c8634b1bf501d0f02c141cf9994829c60936
Eplxs <79092281+Ep-lxs@users.noreply.github.com>
2022-09-11 12:13:08 +0200

    gygyguy

---
+++ modules/util.lua
@@ -0,0 +1,38 @@
+local module = {}
+
+function module:Refresh()
+    module.Players = game:GetService("Players")
+    module.ReplicatedStorage = game:GetService("ReplicatedStorage")
+    module.TeleportService = game:GetService("TeleportService")
+    module.RunService = game:GetService("RunService")
+    module.HttpService = game:GetService("HttpService")
+    module.CoreGui = game:GetService("CoreGui")
+    module.TweenService = game:GetService("TweenService")
+    module.PathfindingService = game:GetService("PathfindingService")
+
+    module.Player = module.Players.LocalPlayer
+    module.Character = module.Player.Character
+    module.Humanoid = module.Character.Humanoid
+    if module.Humanoid ~= nil then
+        module.Health = module.Humanoid.Health
+        module.MaxHealth = module.Humanoid.MaxHealth
+        module.WalkSpeed = module.Humanoid.WalkSpeed
+        module.JumpPower = module.Humanoid.JumpPower
+    end
+
+    function module.Create(ClassName, Properties)
+        local instance = Instance.new(ClassName)
+
+        for property, value in next, Properties do
+            instance[property] = value
+        end
+        
+        return instance
+    end
+end
+
+module.RunService.RenderStepped:Connect(function()
+    module:Refresh()
+end)
+
+return module
