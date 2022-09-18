efb0bc669c21168c2b37c412c60c0ef5598a6ab6
Eplxs <79092281+Ep-lxs@users.noreply.github.com>
2022-09-18 13:49:26 +0200

    huhuhu

--- modules/util.lua
+++ modules/util.lua
@@ -1,5 +1,35 @@
 local module = {}
 
+module.Players = game:GetService("Players")
+module.ReplicatedStorage = game:GetService("ReplicatedStorage")
+module.TeleportService = game:GetService("TeleportService")
+module.RunService = game:GetService("RunService")
+module.HttpService = game:GetService("HttpService")
+module.CoreGui = game:GetService("CoreGui")
+module.TweenService = game:GetService("TweenService")
+module.PathfindingService = game:GetService("PathfindingService")
+
+module.Player = module.Players.LocalPlayer
+module.Mouse = module.Player:GetMouse()
+module.Character = module.Player.Character
+module.Humanoid = module.Character.Humanoid
+if module.Humanoid ~= nil then
+    module.Health = module.Humanoid.Health
+    module.MaxHealth = module.Humanoid.MaxHealth
+    module.WalkSpeed = module.Humanoid.WalkSpeed
+    module.JumpPower = module.Humanoid.JumpPower
+end
+
+function module.Create(ClassName, Properties)
+    local instance = Instance.new(ClassName)
+
+    for property, value in next, Properties do
+        instance[property] = value
+    end
+    
+    return instance
+end
+
 function Refresh()
     module.Players = game:GetService("Players")
     module.ReplicatedStorage = game:GetService("ReplicatedStorage")
@@ -33,8 +63,10 @@ function Refresh()
 end
 
 Refresh()
-module.RunService.RenderStepped:Connect(function()
-    Refresh()
-end)    
+task.spawn(function()
+    while task.wait() do
+        Refresh()
+    end
+end)
 
 return module
