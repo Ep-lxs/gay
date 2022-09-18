ff09472ce488e8f8359dc3752e43bbec08c35be6
Eplxs <79092281+Ep-lxs@users.noreply.github.com>
2022-09-18 13:44:20 +0200

    hyuyuy

--- modules/util.lua
+++ modules/util.lua
@@ -1,43 +1,40 @@
-return function()
-    local module = {}
+local module = {}
 
-    function module:Refresh()
-        module.Players = game:GetService("Players")
-        module.ReplicatedStorage = game:GetService("ReplicatedStorage")
-        module.TeleportService = game:GetService("TeleportService")
-        module.RunService = game:GetService("RunService")
-        module.HttpService = game:GetService("HttpService")
-        module.CoreGui = game:GetService("CoreGui")
-        module.TweenService = game:GetService("TweenService")
-        module.PathfindingService = game:GetService("PathfindingService")
-    
-        module.Player = module.Players.LocalPlayer
-        module.Mouse = module.Player:GetMouse()
-        module.Character = module.Player.Character
-        module.Humanoid = module.Character.Humanoid
-        if module.Humanoid ~= nil then
-            module.Health = module.Humanoid.Health
-            module.MaxHealth = module.Humanoid.MaxHealth
-            module.WalkSpeed = module.Humanoid.WalkSpeed
-            module.JumpPower = module.Humanoid.JumpPower
-        end
-    
-        function module.Create(ClassName, Properties)
-            local instance = Instance.new(ClassName)
-    
-            for property, value in next, Properties do
-                instance[property] = value
-            end
-            
-            return instance
-            
-        end
+function Refresh()
+    module.Players = game:GetService("Players")
+    module.ReplicatedStorage = game:GetService("ReplicatedStorage")
+    module.TeleportService = game:GetService("TeleportService")
+    module.RunService = game:GetService("RunService")
+    module.HttpService = game:GetService("HttpService")
+    module.CoreGui = game:GetService("CoreGui")
+    module.TweenService = game:GetService("TweenService")
+    module.PathfindingService = game:GetService("PathfindingService")
+
+    module.Player = module.Players.LocalPlayer
+    module.Mouse = module.Player:GetMouse()
+    module.Character = module.Player.Character
+    module.Humanoid = module.Character.Humanoid
+    if module.Humanoid ~= nil then
+        module.Health = module.Humanoid.Health
+        module.MaxHealth = module.Humanoid.MaxHealth
+        module.WalkSpeed = module.Humanoid.WalkSpeed
+        module.JumpPower = module.Humanoid.JumpPower
     end
-    
-    module:Refresh()
-    module.RunService.RenderStepped:Connect(function()
-        module:Refresh()
-    end)    
 
-    return module
+    function module.Create(ClassName, Properties)
+        local instance = Instance.new(ClassName)
+
+        for property, value in next, Properties do
+            instance[property] = value
+        end
+        
+        return instance
+    end
 end
+
+Refresh()
+module.RunService.RenderStepped:Connect(function()
+    Refresh()
+end)    
+
+return module
