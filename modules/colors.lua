775c9a0addaa9919bf1cf68f7525d209e13a1f3b
Eplxs <79092281+Ep-lxs@users.noreply.github.com>
2022-12-26 03:10:25 +0100

    Delete colors.lua

--- modules/colors.lua
+++
@@ -1,8 +0,0 @@
-local module = {
-    accent = Color3.fromRGB(49, 106, 212),
-    frame_bg = Color3.fromRGB(32, 32, 32),
-    frame_bg_selected = Color3.fromRGB(37, 37, 37),
-    fg = Color3.fromRGB(244, 244, 244),
-}
-
-return module
